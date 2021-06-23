410aa44a2276eccb75d60e3369c6263877291356
Afonso -aptsharp <aff-on@hotmail.com>
2021-06-22 22:43:43 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MasterComponent } from './pages/master/master.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    MasterComponent,
+    HomeComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
