896fc176ad5341248bfe22a66393244577e96fa9
Afonso -aptsharp <aff-on@hotmail.com>
2021-06-23 00:11:05 -0300

    finalização.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ReportsComponent } from "./pages/reports/reports.component";
+import { MasterComponent } from "./pages/master/master.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  {
+      path: '',
+      component: MasterComponent,
+      children: [
+      { path: '', component: HomeComponent },
+      { path: 'reports', component: ReportsComponent },
+      ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  txt = 'teste';
 
   ngOnInit() {
+
+    const par = this.activatedRoute.snapshot.paramMap.get('parametro');
+    console.log(par);
+
+  }
+
+  goToLogin(){
+    //this.router.navigate(['/login']);
   }
 
 }
